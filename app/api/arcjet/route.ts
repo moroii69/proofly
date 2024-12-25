ac3f0ce014f2161bf450958e714bdfd449ca731c
ufraaan <ufraansiddiqui30@gmail.com>
2024-12-26 00:32:34 +0530

    feat: add ArcJet integration for request protection and remove outdated documentation pages

---
+++ app/api/arcjet/route.ts
@@ -0,0 +1,34 @@
+import arcjet, { shield } from "@arcjet/next";
+import { NextResponse } from "next/server";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    shield({
+      mode: "DRY_RUN",
+    }),
+  ],
+});
+
+export async function GET(req: Request) {
+  const decision = await aj.protect(req);
+
+  for (const result of decision.results) {
+    console.log("Rule Result", result);
+  }
+
+  console.log("Conclusion", decision.conclusion);
+
+  if (decision.isDenied() && decision.reason.isShield()) {
+    return NextResponse.json(
+      {
+        error: "You are suspicious!",
+      },
+      { status: 403 },
+    );
+  }
+
+  return NextResponse.json({
+    message: "Hello world",
+  });
+}
