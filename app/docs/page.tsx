99170ccd4618529a31150947ebf11c6c21239e56
ufraaan <ufraansiddiqui30@gmail.com>
2024-12-25 23:31:11 +0530

    testing subdomain for /docs

---
+++ app/docs/page.tsx
@@ -0,0 +1,11 @@
+import { FooterSection } from "@/components/layout/sections/footer";
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <FooterSection />
+    </div>
+  );
+}
+
